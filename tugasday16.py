866b558a28da24ed9e35614bb9ad3e3f4ff88023
Dinar Jaya Pertela <134934832+dinarjy@users.noreply.github.com>
2023-05-30 22:10:11 +0700

    Add files via upload

---
+++ tugasday16.py
@@ -0,0 +1,58 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+
+class TestLogin(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+ 
+    def test_success_login(self):
+        # testcase 1
+        driver = self.browser #buka web browser
+        driver.get("https://www.saucedemo.com/") # buka situs
+        time.sleep(3)
+        driver.find_element(By.ID,"user-name").send_keys("standard_user") # isi username
+        time.sleep(1)
+        driver.find_element(By.ID,"password").send_keys("secret_sauce") # isi password
+        time.sleep(1)
+        driver.find_element(By.ID, "login-button").click()
+        time.sleep(1)
+
+    def test_failed_login(self):
+        # testcase 2
+        driver = self.browser #buka web browser
+        driver.get("https://www.saucedemo.com/") # buka situs
+        time.sleep(3)
+        driver.find_element(By.ID,"user-name").send_keys("kosongkan") # isi username
+        time.sleep(1)
+        driver.find_element(By.ID,"password").send_keys("kosongkan") # isi password
+        time.sleep(1)
+        driver.find_element(By.ID, "login-button").click()
+        time.sleep(1)
+
+        # validasi
+        error_massage = driver.find_element(By.CSS_SELECTOR, "[data-test='error']").text
+        self.assertIn('Epic sadface: Username and password do not match any user in this service', error_massage)
+    
+    def test_success_login(self):
+        # testcase 1
+        driver = self.browser #buka web browser
+        driver.get("https://www.saucedemo.com/") # buka situs
+        time.sleep(3)
+        driver.find_element(By.ID,"user-name").send_keys("problem_user") # isi username
+        time.sleep(1)
+        driver.find_element(By.ID,"password").send_keys("secret_sauce") # isi password
+        time.sleep(1)
+        driver.find_element(By.ID, "login-button").click()
+        time.sleep(1)
+    
+    def tearDown(self):
+        self.browser.close()
+        
+if __name__ == "__main__":
+    unittest.main()
